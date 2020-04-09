1a304f0624d7b3003be2b30c2f6c4a2255be10ec
zhanqi <zhanqi.me@gmail.com>
2020-04-09 21:08:20 +0800

    INIT

---
+++ components/hello/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    <slot v-if="loaded"></slot>
+    <view v-else>正在加载</view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props:['loaded']
+  }
+</script>
+
+<style>
+</style>
